dad4d7069e9aa2c59b9ec83ca290c0d59cb4c42a
Minsang <minsang1233@gmail.com>
2025-09-14 19:26:13 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 564 ms, Memory: 147516 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -5,40 +5,26 @@ import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 class Main {
-    static int N;
-    static ArrayList<Integer>[] LIS;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-
-        LIS = new ArrayList[N+1];
-
-        for(int i = 1; i <= N; i++){
-            LIS[i] = new ArrayList<>();
-        }
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= N; i++){
-            int num = Integer.parseInt(st.nextToken());
-            LIS[i].add(num);
-        }
-
+        int N = Integer.parseInt(br.readLine());
+        
         ArrayList<Integer> length = new ArrayList<>();
-
-        for(int i = 1; i <= N; i++){
-            int num = LIS[i].get(0);
-
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            int num = Integer.parseInt(st.nextToken());
+            
             if(length.isEmpty() || length.get(length.size()-1) < num){
                 length.add(num);
             } else {
                 int left = 0;
                 int right = length.size() - 1;
-
+                
                 while(left < right){
                     int mid = (left + right) / 2;
-
+                    
                     if(length.get(mid) >= num){
                         right = mid;
                     } else {
@@ -48,8 +34,7 @@ class Main {
                 length.set(right, num);
             }
         }
-
+        
         System.out.println(length.size());
     }
-
 }
