b96e67caa234a7a9f33d15bdcb9cb6bd8d5df22b
Minsang <minsang1233@gmail.com>
2025-09-14 17:27:00 +0900

    [Gold III] Title: 택배, Time: 268 ms, Memory: 18868 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.java
+++ 백준/Gold/1719. 택배/택배.java
@@ -1,104 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Main {
+class Main{
     static int n, m;
-    static ArrayList<Point>[] graph;
-    static int[][] result;
-    static int INF = Integer.MAX_VALUE;
+    static int[][]  next;
+    static int[][] dist;
+    static int INF = 987654321;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[n+1];
-        result = new int[n+1][n+1];
+        next = new int[n+1][n+1];
+        dist = new int[n+1][n+1];
 
         for(int i = 1; i <= n; i++){
-            graph[i] = new ArrayList<>();
-            
-        }
-
-        for(int i = 0; i < m; i++){
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-
-        graph[start].add(new Point(end, weight));
-        graph[end].add(new Point(start, weight));
-
-        }
-        
-        for(int i = 1; i <= n; i++){
-            dij(i);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <=n; i++){
             for(int j = 1; j <= n; j++){
-                if(i == j) {
-                    sb.append("- ");
-            } else {
-                sb.append(result[i][j]).append(" ");
-            }
-        }
-        sb.append("\n");
-    }
-
-    System.out.println(sb.toString());;
-}
-
-        static void dij(int start){
-            int[] dist = new int[n+1];
-            Arrays.fill(dist, INF);
-            dist[start] = 0;
-            
-            PriorityQueue<Point> pq = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
-            pq.add(new Point(start, 0));
-
-            while(!pq.isEmpty()){
-                Point cur = pq.poll();
-
-                int now = cur.to;
-                int newCost = cur.cost;
-
-                if(newCost > dist[now]) continue;
-
-                for(Point p : graph[now]){
-                    int nextTo = p.to;
-                    int nextCost = dist[now] + p.cost;
-
-                    if(nextCost < dist[nextTo]){
-                        dist[nextTo] = nextCost;
-                        
-                        if(now == start){
-                            result[start][nextTo] = nextTo;
-                        } else {
-                            result[start][nextTo] = result[start][now];
-                        }
-                        
-                        pq.add(new Point(nextTo, nextCost));
+                if(i == j){
+                    dist[i][j] = 0;
+                } else {
+                    dist[i][j] = INF;
                 }
             }
         }
-    }
 
-    static class Point{
-        int to;
-        int cost;
-
-        Point(int to, int cost){
-            this.to = to;
-            this.cost = cost;
+        for(int i = 0; i < m; i++){
+            st= new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            
+            if(dist[start][end] > weight){
+                dist[start][end] = weight;
+                dist[end][start] = weight;
+                next[start][end] = end;
+                next[end][start] = start;
+            }
         }
+
+
+        for(int k = 1; k <= n; k++){
+            for(int i = 1; i <= n; i++){
+                for(int j = 1; j <= n; j++){
+                    if(dist[i][j] > dist[i][k] + dist[k][j]){
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                        next[i][j] = next[i][k];
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == j){
+                    sb.append("- ");
+                } else {
+                    sb.append(next[i][j]).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb.toString());
+
     }
 }
