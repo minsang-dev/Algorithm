c4875640bcec9781add6077e6754588c8cfebbed
Minsang <minsang1233@gmail.com>
2025-11-14 22:01:10 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int y = Integer.parseInt(br.readLine());
+
+        System.out.println(y - 543);
+
+    }
+}
