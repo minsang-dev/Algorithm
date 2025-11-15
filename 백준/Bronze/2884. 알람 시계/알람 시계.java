05370ad47ec8d954630d898ce58d6d446d2e0c7f
Minsang <minsang1233@gmail.com>
2025-11-15 23:19:12 +0900

    [Bronze III] Title: 알람 시계, Time: 64 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if(H == 0 && M < 45){
+            System.out.println(23 + " " + (M+15));
+        } else if(M < 45){
+            System.out.println((H-1) + " " + (M+15));
+        } else {
+            System.out.println(H + " " + (M-45));
+        }
+    }
+}
