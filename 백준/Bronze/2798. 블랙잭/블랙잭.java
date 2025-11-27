988abd675811d549d701ea0c283de07f298c9b2c
Minsang <minsang1233@gmail.com>
2025-11-27 21:40:59 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 11628 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -3,39 +3,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main{
-    static int N, M;
-    static int gapMax = 0;
-    static int[] arr;
+public class Main {
+	static int N, M;
+	// 카드 수 : N장, 숫자 : M
+	static int[] A;
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()); 
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		A = new int[N];
+		int max = 0;
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i = 0; i < N-2; i++) {
+			for(int j = i+1; j < N-1; j++) {
+				for(int k = j+1; k < N; k++) {
+					int sum = A[i] + A[j] + A[k]; // 세 수 합
+					
+					if(sum <= M) {
+						max = Math.max(max, sum);
+					}
+				}
+			}
+			
+		}
+		System.out.println(max);
+	}
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        arr = new int[N];
-
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        dfs(0, 0, 0);
-        System.out.println(gapMax);
-    }
-
-
-    static void dfs(int depth, int start, int sum){
-        if(depth == 3){
-            if(sum <= M){
-                gapMax = Math.max(gapMax, sum);
-            }
-            return;    
-        }
-        for(int i = start; i < N; i++){
-        dfs(depth+1, i+1, sum + arr[i]);
-            }    
-        
-    }
 }
