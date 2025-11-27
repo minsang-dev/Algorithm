5460c9529911c948a709d4404ec05e754cefb68f
Minsang <minsang1233@gmail.com>
2025-11-27 20:05:47 +0900

    [Bronze II] Title: 분해합, Time: 100 ms, Memory: 11600 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,34 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-class Main{
-    static int N;
-    static int result;
+public class Main {
+	static int N, M;
 
-    public static void main (String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(st.nextToken());
+		// 자연수 M의 분해합이 N
+		N = Integer.parseInt(br.readLine());
+		
+		int answer = 0;
+		
+		// N까지의 숫자까지 다 돌아봅니다.
+		for(int i = 1; i <= N; i++) {
+			int sum = i;
+			int tmp = i;
+			
+			// 숫자 쪼갤건데 0보다 커야하고
+			while(tmp > 0) {
+				// 숫자 하나씩 쪼개기
+				sum += tmp%10;
+				tmp /= 10;
+			}
+			
+			// sum == N -> answer = i;
+			if(sum == N) {
+				answer = i;
+				break;
+			}
+		}
+		
+		System.out.println(answer);
+	}
 
-        int num = 0;
-        int M = 0;
-
-        for(int i = 0; i < N; i++){
-            num = i;
-            int sum = 0;
-
-            while(num != 0){
-                sum += num%10;
-                num /= 10;
-            }
-            if(sum+i == N){
-                M = i;
-                break;
-            }
-        }
-        System.out.println(M);
-    }
 }
