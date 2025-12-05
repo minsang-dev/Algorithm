a6cac290f45c0c6464e89b1ac92a9a9bfb04135b
Minsang <minsang1233@gmail.com>
2025-12-05 20:46:43 +0900

    [Bronze III] Title: 최댓값, Time: 64 ms, Memory: 11468 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] arr = new int[9];
+		int max = 0;
+		int count = 0;
+		
+		for(int i = 0; i < 9; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			
+			if(arr[i] > max) {
+				max = arr[i];
+				count = i+1;
+			}
+			
+		}
+		
+		System.out.println(max);
+		System.out.println(count);
+			
+
+	}
+
+}
