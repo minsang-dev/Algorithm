61499b41f89d7c5ce29103a19ec8944ff2062988
Minsang <minsang1233@gmail.com>
2025-12-07 15:56:10 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1536 ms, Memory: 480708 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -7,29 +7,47 @@ import java.util.Collections;
 
 public class Main {
 	static int N;
-	static int[] arr;
+//	static int[] arr;
 	// 메모리 초과 ---> arrays.sort
 	// 이번엔 list -> collection.sort 로 안통하다니 ..
+	// arrays.sort로 바꿔서 풀었더니 가능 !
+	// 하지만,,, 나는 counting sort로도 풀어봐야지 ~
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
 		N = Integer.parseInt(br.readLine());
-
-		arr = new int[N];
 		
-		for(int i =0 ; i < N; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		Arrays.sort(arr);;
+		int[] count = new int[10001];
 		
 		for(int i = 0; i < N; i++) {
-			sb.append(arr[i]).append("\n");
+			count[Integer.parseInt(br.readLine())]++;
+		}
+		
+		for(int i = 1; i <= 10000; i++) {
+			for(int j = 0; j < count[i]; j++) {
+				sb.append(i).append("\n");
+			}
 		}
 		
 		System.out.println(sb);
+
+//		arr = new int[N];
+//		
+//		for(int i =0 ; i < N; i++) {
+//			arr[i] = Integer.parseInt(br.readLine());
+//		}
+//		
+//		Arrays.sort(arr);;
+//		
+//		for(int i = 0; i < N; i++) {
+//			sb.append(arr[i]).append("\n");
+//		}
+//		
+//		System.out.println(sb);
+		
+		
 	}
 
 }
